87236fafdc3c3e960339d47cc65146c824c36918
Gustasilvadev <gustamarsil@gmail.com>
2025-11-03 09:50:45 -0300

    Commit inicial primeira tela

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/logo.png')} />
+      <Image source={require('../assets/images/imagem_inicial.png')} />
+      <Text>Boas Vindas!</Text>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex:1,
+    backgroundColor:'#00457D',
+    alignItems:"center",
+    gap:40,
+  }
+
+});
